766ea34ad4893047d81794f4ca1dcb619fd97c6c
brian.zheng <brian.zheng@oocl.com>
2018-11-20 09:00:02 +0800

    initial

--- goroutine.go
+++ goroutine.go
@@ -1,14 +1,12 @@
 package main
 
-import (
-	"time"
-)
+import "time"
 
 func double(a int) int { return a * 2 }
 
 func main() {
-
-	for i := 0; i < 100000; i++ {
+	//是三十万，不用数了...
+	for i := 0; i < 300000; i++ {
 		go println(double(i))
 	}
 	time.Sleep(time.Second * 1)
