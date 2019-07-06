91b8f2d71f434e41911d6187396b343d85f81874
brian.zheng <brian.zheng@oocl.com>
2019-07-06 10:12:04 +0800

    xxx

--- goroutine.go
+++ goroutine.go
@@ -9,8 +9,8 @@ func double(a int) int { return a * 2 }
 func main() {
 
 
-	for i := 0; i < 300000; i++ {
+	for i := 0; i < 2000000; i++ {
 		go println(double(i))
 	}
-	time.Sleep(time.Second * 1)
+	time.Sleep(time.Second * 15)
 }
